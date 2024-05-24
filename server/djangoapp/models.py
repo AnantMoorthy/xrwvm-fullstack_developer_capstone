922672162f8ae5befe36d1b223778b1b8f9ea7af
AnantMoorthy <moorthyanant@gmail.com>
2024-05-24 14:39:07 -0400

    changes made to add and retrieve car details

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -13,15 +13,35 @@ class CarMake(models.Model):
 # - Description
     description = models.TextField()
 # - Any other fields you would like to include in car make model
+    VIN = models.CharField(max_length=17)
 # - __str__ method to print a car make object
+    def __str__(self):
+        return self.name # Return the name as the string representation
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
+class CarModel(models.Model):
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
 # - Name
+    name = models.CharField(max_length=100)
 # - Type (CharField with a choices argument to provide limited choices
 # such as Sedan, SUV, WAGON, etc.)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('RACE', 'race'), 
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
 # - Year (IntegerField) with min value 2015 and max value 2023
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+    def __str__(self):
+        return self.name  # Return the name as the string representation
